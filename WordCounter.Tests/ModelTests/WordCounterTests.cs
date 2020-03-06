12f255758b6eb4c16ee1df4501190eafc3c3e51e
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-06 09:11:28 -0800

    fix errors in code and testing by uncommenting out sentence and inputting parameters in test

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -9,7 +9,7 @@ namespace WordCounter.Tests
     [TestMethod]
     public void RepeatCounter_ReturnInputWord_Word()
     {
-      RepeatCounter newCounter = new RepeatCounter("test");
+      RepeatCounter newCounter = new RepeatCounter("test", "this is a sentence");
       string result = newCounter.GetWord();
       Assert.AreEqual(result, "test");
     }
