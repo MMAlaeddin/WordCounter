3d226392109925670d650c29bfa4b399cac15867
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-06 09:06:33 -0800

    initial fail for GetWord

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -2,3 +2,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter.Models;
 
 namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTests
+  {
+    [TestMethod]
+    public void RepeatCounter_ReturnInputWord_Word()
+    {
+      RepeatCounter newCounter = new RepeatCounter("test");
+      string result = newCounter.GetWord();
+      Assert.AreEqual(result, "test");
+    }
+  }
+}
