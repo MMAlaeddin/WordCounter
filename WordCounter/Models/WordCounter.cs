20fab817a3252c134c68653fd2713167152f20dd
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-06 09:37:23 -0800

    fix contains method

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,10 +5,21 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
-    public string Word 
-    public string Sentence
-
+    public string Word;
+    public string Sentence;
+  
     public RepeatCounter(string inputWord, string inputSentence)
-
+    {
+      Word = inputWord;
+      Sentence = inputSentence;
+    }
+    public string GetWord()
+    {
+      if (Word.ToCharArray().Contains('1', '2', '3', '4', '5', '6', '7', '8', '9', '0'))
+      {
+        return "wrong";
+      }
+      return Word;
+    }
   }
 }
