bec2febb6cb0365ddd0d25890602b1eac56ba3ca
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-06 15:48:03 -0800

    add basic ui

---
+++ WordCounter/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using WordCounter.Models;
+
+namespace WordCounter
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the Word Counter!");
+      Console.WriteLine("Please enter a word");
+      string newWord = Console.ReadLine();
+      Console.WriteLine("Please ender a sentence");
+      string newSentence = Console.ReadLine();
+      RepeatCounter newCounter = new RepeatCounter(newWord, newSentence);
+      int finalOutput = newCounter.CountWord();
+      Console.WriteLine(newWord + " repeats " + finalOutput + " time(s) in your sentence.");
+    }
+  }
+}
